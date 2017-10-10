6bb6dd8355e2d07d62efbeb0efdfdfdaf70f042d
sophiasun0515 <ruixuansun@gatech.edu>
2017-10-10 13:29:40 -0400

    first commit

---
+++ ShoppingList/ListItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ListItemCell.swift
+//  ShoppingList
+//
+//  Created by Apple on 10/3/17.
+//  Copyright © 2017 Sophia's Secret Garden. All rights reserved.
+//
+
+import UIKit
+
+class ListItemCell: NSObject {
+
+}
